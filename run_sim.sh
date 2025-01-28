4481f47fe6392040354ff943fc9e5d46c4d15cb5
chirag-parmar <chiragparmar12209@gmail.com>
2025-01-28 21:23:21 +0530

    review fix

--- run_sim.sh
+++ run_sim.sh
@@ -6,7 +6,7 @@ for kb in 128 256 512 1024 2048 4096 8192; do
   for announce in 0 7 8; do
       result=$((kb * 1024))
       filename=shadow-$kb-$announce-1
-      python3 network_graph.py 100 70 $result $num_msgs 8 $announce
+      python3 network_graph.py 5000 35 $result 1 8 $announce
 
       shadow -d $filename.data shadow.yaml
 
@@ -19,9 +19,9 @@ done
 
 for announce in 0 7 8; do
   for num_msgs in 2 4 8 16 32 64; do
-    result=$((kb * 1024))
+    result=$((128 * 1024))
     filename=shadow-128-$announce-$num_msgs
-    python3 network_graph.py 100 70 $result $num_msgs 8 $announce
+    python3 network_graph.py 5000 35 $result $num_msgs 8 $announce
 
     shadow -d $filename.data shadow.yaml
 
