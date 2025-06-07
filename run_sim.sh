83da12eac5aec75504df43166f5e71b2bec071b2
Suphanat Chunhapanya <haxx.pop@gmail.com>
2025-06-07 23:12:36 +0700

    fixup! Chunked PeerDAS

--- run_sim.sh
+++ run_sim.sh
@@ -10,12 +10,13 @@ D=8
 
 for num_blobs in 512 256 128 64 32; do
     kbs=$((2 * $num_blobs)) # each cell of the column is 2KB
-    for chunk in $kbs $((32 * 1024)); do
-        result=$(($kbs * 1024))
+    for chunk in $kbs 32; do
+        column_size=$(($kbs * 1024))
+        chunk_size=$(($chunk * 1024))
         filename=shadow-$kbs-chunk-$chunk
         interval=700
 
-        python3 network_graph.py $num_nodes $conns $result 128 $D $interval 0 $chunk
+        python3 network_graph.py $num_nodes $conns $column_size 128 $D $interval 0 $chunk_size
 
         shadow --progress true -d $filename.data shadow.yaml
 
